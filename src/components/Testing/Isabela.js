ba8b4f8994ee2add88f4447e6ce833c42ce891d0
paulazoo <paulazhu@college.harvard.edu>
2020-08-12 21:16:08 -0400

    Teach Isabela redux later

--- src/components/Testing/Isabela.js
+++ src/components/Testing/Isabela.js
@@ -49,7 +49,7 @@ I will teach you how Redux works later */
 // Redux
 /* here we are importing the connect function that 
 we will use at the end to connect this component to the global redux store */
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 /* Theme and styling related imports */
 // Theme
@@ -62,8 +62,8 @@ we name this function "useStyles" */
 const useStyles = makeStyles((theme) => ({
   exampleClass: {
     color: 'red',
-    backgroundColor: 'blue'
-  }
+    backgroundColor: 'blue',
+  },
 }));
 
 /* this is the start of our actual functional component
@@ -73,17 +73,18 @@ we also input in some property arguments with:
 Isabela(props)
 "props" stands for properties
 */
-export default function Isabela(props) {
+function Isabela(props) {
   /* This 1st part of the component is where we write the logic and javascript of the component */
 
   /* first we get the css styling classes (as the constant "classes") 
-  from the useStyles function that we made above */ 
+  from the useStyles function that we made above */
+
   const classes = useStyles();
 
   /* for React components, we use the React hook "useState" 
   this is because we can't actually just set variables like textfieldValue = 'hoaiuhf' that will not work
   instead React uses states so we must use the setTextfieldValue function to assign a new value to textfieldValue */
-  const [textfieldValue, setTextfieldValue] = useState('initial value')
+  const [textfieldValue, setTextfieldValue] = useState('initial value');
   /* the syntax for useState is like this:
   const [the variable itself, the function for changing the variable] = useState(the initial value of the variable)
   */
@@ -95,8 +96,8 @@ export default function Isabela(props) {
     /* console.log function will print things out into the console of the website
     you probably already know this but
     the console of the website can be accessed by Ctrl+Shift+I when on the website tab */
-    console.log("print me to the console")
-  }
+    console.log('print me to the console');
+  };
 
   /* this is another javascript function
   this function I want to run every time the textfield (below) is typed in
@@ -105,51 +106,49 @@ export default function Isabela(props) {
   const handleTextfieldValueChange = (event) => {
     /* here I am setting the textfield value (using the setTextfieldValue function)
      to whatever the user typed into the textfield (event.target.value) */
-    setTextfieldValue(event.target.value)
-  }
-
+    setTextfieldValue(event.target.value);
+  };
 
   /* This 2nd part of the component is where we write the html 
   that actually gets returned and shows up on the website */
   return (
     <>
-    {/* Material UI's Typography displays text 
+      {/* Material UI's Typography displays text 
     (and automatically uses our theme's font and spacing and everything) */}
-    <Typography>
-      Some text blah blah blah
-    </Typography>
-    {/* Material UI's box
+      <Typography>Some text blah blah blah</Typography>
+      {/* Material UI's box
     we can use a custom css styling class by setting
     className={classes.exampleClass}
     exampleClass is a class we made in the beginning using makeStyles */}
-    <Box className={classes.exampleClass}>
-      This is a box
-    </Box>
-    {/* Here we have a Material UI button
+      <Box className={classes.exampleClass}>This is a box</Box>
+      {/* Here we have a Material UI button
     it is our theme's primary color
     when the button is clicked, it will run the handleButtonClick javascript function */}
-      <Button color='primary' variant='contained' onClick={handleButtonClick}>Button Text</Button>
+      <Button color='primary' variant='contained' onClick={handleButtonClick}>
+        Button Text
+      </Button>
       {/* Here we have a Material UI textfield 
       its value is the variable textfieldValue
       when you try to change the textfield value by typing in it
       it will run the javascript handleTextfieldValueChange function
       */}
-      <TextField variant='outlined' value={textfieldValue} onChange={handleTextfieldValueChange}>
-      </TextField>
+      <TextField
+        variant='outlined'
+        value={textfieldValue}
+        onChange={handleTextfieldValueChange}
+      />
     </>
   );
 }
 
+/* the rest of the code is for connecting the component to the global Redux state
+i will teach you about Redux later */
 const mapStateToProps = () => {
-  return {
-    
-  }
-}
+  return {};
+};
 
 const mapDispatchToProps = () => {
-  return {
+  return {};
+};
 
- }
-}
-
-export default connect(maptStateToProps, mapDispatchToProps)(Isabela)
+export default connect(maptStateToProps, mapDispatchToProps)(Isabela);
