86ac5e7990f54852ad35ebecbe0bffcd89edf968
paulazoo <paulazhu@college.harvard.edu>
2020-10-20 13:30:24 -0400

    functional paypal component, use client id in env

--- src/components/Donate/PaypalDonate.js
+++ src/components/Donate/PaypalDonate.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { PayPalButton } from 'react-paypal-button-v2';
 
-class PaypalDonate extends React.Component {
-  render() {
-    return (
-      <PayPalButton
-        amount={0.01}
-        currency='USD'
-        onSuccess={(details, data) => console.log(details, data)}
-        options={{
-          clientId:
-            'AQulV7aFCM-Ki5udsLsyCpAS6X76uyx2XKh-NXo9Tk25wBU3UHa6fovT6FKINDZ6xPOYTHZs2HQaA0H5',
-        }}
-      />
-    );
-  }
+function PaypalDonate(props) {
+  return (
+    <PayPalButton
+      amount={0.01}
+      currency='USD'
+      onSuccess={(details, data) => console.log(details, data)}
+      options={{
+        clientId: process.env.REACT_APP_PAYPAL_CLIENT_ID,
+      }}
+    />
+  );
 }
 export default PaypalDonate;
