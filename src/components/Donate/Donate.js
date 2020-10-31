658532fdecd4a5264ddb118ce7f96010f81b2ae1
paulazoo <paulazhu@college.harvard.edu>
2020-10-31 00:27:34 -0400

    donation message

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -1,11 +1,29 @@
-import { Button, Grid, TextField, InputAdornment } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  TextField,
+  InputAdornment,
+  Typography,
+} from '@material-ui/core';
 import React, { useState } from 'react';
+
+// Theme
+import { makeStyles } from '@material-ui/core/styles';
 
 // Custom Components
 import PaypalDonate from './PaypalDonate';
 import Navbar from '../Navbar/Navbar';
 
+const useStyles = makeStyles((theme) => ({
+  donationMessage: {
+    textAlign: 'center',
+    padding: '30px !important',
+  },
+}));
+
 export default function Donate() {
+  const classes = useStyles();
+
   const [donateAmount, setDonateAmount] = useState('0.0');
 
   const handleDonateAmount = (e) => {
@@ -22,8 +40,15 @@ export default function Donate() {
         justifyContent='center'
         spacing={3}
       >
-        <Grid item xs={12}>
-          <p>plsss donate monies bc we are pooor</p>
+        <Grid item xs={12} className={classes.donationMessage}>
+          <Typography>
+            <b>We need your help to keep this nonprofit running! 🥺</b>
+          </Typography>
+          <Typography>
+            Your donation will be used to cover the costs of programming for our
+            fellows, software costs, filing expenses, server and database
+            hosting, data storage, graphics, and website functionality.
+          </Typography>
         </Grid>
         <Grid item xs={12}>
           <TextField
--- src/components/Donate/PaypalDonate.js
+++ src/components/Donate/PaypalDonate.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import { PayPalButton } from 'react-paypal-button-v2';
 
+import Swal from 'sweetalert2';
+
 function PaypalDonate({ donateAmount }) {
   const handleSuccess = (details, data) => {
     console.log(details, data);
+    Swal.fire({
+      title: 'THANK YOU',
+      text:
+        'College ARCH (Application Resources for College and Higher-education) is able to help kids because of people like you. 💞 From the team here at College ARCH, thank you!',
+    });
   };
 
   return (
@@ -17,7 +24,7 @@ function PaypalDonate({ donateAmount }) {
           clientId: process.env.REACT_APP_PAYPAL_CLIENT_ID,
         }}
       />
-      <Button onClick={handleSuccess('dtails', 'data')}>Simulate</Button>
+      <Button onClick={() => handleSuccess('dtails', 'data')}>Simulate</Button>
     </>
   );
 }
