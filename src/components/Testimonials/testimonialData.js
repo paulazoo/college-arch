6298fa6630166d17ea498c4721555dc15966d7ae
paulazoo <paulazhu@college.harvard.edu>
2021-02-14 22:42:08 -0500

    insert testimonial dat

--- src/components/Testimonials/testimonialData.js
+++ src/components/Testimonials/testimonialData.js
@@ -1,7 +1,7 @@
 export const testimonialData = [
   {
     source: null,
-    author: 'Sophia Singer',
+    author: 'Sophia',
     content:
       'Just from one session it already helps me sm in knowing how to prepare for all this so just know how appreciative we all are for you guys and this opportunity!!',
   },
@@ -14,18 +14,54 @@ export const testimonialData = [
     source: null,
     author: 'Anonymous',
     content:
-      'Muchas gracias por todo lo que hacen, son de demasiada ayuda!! <33',
+      'Soy mentee en [College ARCH], muchas gracias por todo lo que hacen, son de demasiada ayuda!! <33',
   },
   {
     source: null,
-    author: 'Reycajen Magbulos',
+    author: 'Reycajen',
     content:
       "This honestly feels like such a prestigious program though. I genuinely didn't think that I'd get accept into any university before all this. Thank you for saving me from being a rice farmer <3",
   },
   {
     source: null,
-    author: 'Reycajen Magbulos',
+    author: 'Lizzy',
     content:
-      "This honestly feels like such a prestigious program though. I genuinely didn't think that I'd get accept into any university before all this. Thank you for saving me from being a rice farmer <3",
+      'Thank you for your time and thank you for being part of what makes the [College ARCH] work. You have no idea how hopeful and motivated this program has made me.',
+  },
+  {
+    source: null,
+    author: 'Sophia',
+    content:
+      "I doubt I would've gotten into anything besides my community college without you guys fr [sic]. I'm learning sooo much and finally understanding and developing a plan and able to lay out everything I need to do to stay on track. I went from thinking community college was my only option (still is an option) to being able to consider top 10s and everything in between",
+  },
+  {
+    source: null,
+    author: 'McCane',
+    content:
+      "I'm glad I was able to get this opportunity, thank you mentors :,)",
+  },
+  {
+    source: null,
+    author: 'Isabela',
+    content:
+      "I was afraid of all the [college admissions] process but my mentor, all your panels and your advice are making this process funnier than stressing... Today I feel free to say that I'm in the right path to my goal. You shared with us college essay writing experiences and despite all my doubts, I have practicing with all your resources, the diagnostic exam and re-watching the SAT prep class, right now I'm really happy because I received all the support of my mentor... Thanks for creating this program, I hope you can still grow and share all this knowledge all over the world!",
+  },
+  {
+    source: null,
+    author: 'Seya',
+    content:
+      'Honestly Luis [College ARCH Director] I love roasting ppl [sic] so I wanted to say your presentaiton was trash but it was actually helpful so thank you and pls keep up the great work',
+  },
+  {
+    source: null,
+    author: 'Anthony',
+    content:
+      "This whole process is realy working like a well-oiled machine. I'm honestly amazed how professional and organized everyone in [College ARCH] is!",
+  },
+  {
+    source: null,
+    author: 'Carissa',
+    content:
+      "I know it's been said before but I'll say it again. You guys did such a GREAT job picking our mentors! My mentor gave such helpful advice and we have so much in common. I definitly can see us keeping in touch beyond this program. Thank you!!",
   },
 ];
