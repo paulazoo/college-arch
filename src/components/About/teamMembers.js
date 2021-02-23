348f9f9900744775fb3f9165e1fa3e9d94abf0b3
paulazoo <paulazhu@college.harvard.edu>
2021-02-22 21:58:26 -0500

    south atlantic

--- src/components/About/teamMembers.js
+++ src/components/About/teamMembers.js
@@ -72,7 +72,7 @@ export const recruitmentDirectors = [
   },
   {
     name: 'Anthony Ciano',
-    position: 'South and Mid-Atlantic Regional Director',
+    position: 'South Atlantic Regional Director',
     text:
       "Anthony, part of NC State's class of 2024, is studying human biology and minoring in Spanish. He is excited to aid as many students as he can by extending outreach and ensuring equity within his region. Regardless of socioeconomic status or background, he is dedicated to College Arch’s mission and is eager to work with as many students that he can so they can achieve their personal and academic goals.",
     image: '',
