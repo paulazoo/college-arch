aafa9eb5960345d6cf65bb332880454d382b6b6e
paulazoo <paulazhu@college.harvard.edu>
2023-05-02 00:05:44 -0400

    essay writing tips

--- src/components/Resources/EssayWritingTips.js
+++ src/components/Resources/EssayWritingTips.js
@@ -36,34 +36,24 @@ export default function EssayWritingTips() {
         <ul>
           <li className={classes.infoList}>
             <Typography>
-              In writing, your first couple words have a disproportionate amount
-              of influence.
+              Choose your first words carefully
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
-              One common trick is starting with "I" to immediately give a more
-              personal nuance. Imagine you’re casually telling this story to a
-              friend while they’re sitting next to you.
+              <b>Show, don’t tell!</b>
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
-              <b>Show, don’t tell!!!!!!!!</b>
+              Try not to explicitly say what you or any character is
+              feeling. Use imagery or their actions or other stuff to show;
+              leave something for the reader to infer.
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
-              Almost NEVER explicitly tell me what you or any character is
-              feeling. Use imagery or their actions or other stuff to SHOW me.
-              Don’t just do all the work and force feed everything to the
-              reader; leave some for the reader to infer.
-            </Typography>
-          </li>
-          <li className={classes.infoList}>
-            <Typography>
-              That said, there's a delicate balance between subtlety and
-              clarity. A new reader should still be able to understand or infer
+              That said, a new reader should still be able to easily understand or infer
               the meaning of every sentence.
             </Typography>
           </li>
@@ -78,7 +68,7 @@ export default function EssayWritingTips() {
               Don’t be overly verbose. The best writers pack the most meaning
               and emotion in the least amount of words.
 {' '}
-              <strike>Read Hemingway.</strike>
+              <strike>Read Hemingway because he's awesome.</strike>
             </Typography>
           </li>
           <li className={classes.infoList}>
@@ -90,7 +80,7 @@ export default function EssayWritingTips() {
           </li>
           <li className={classes.infoList}>
             <Typography>
-              You're not running for president, so don’t use generalizations or
+              Don’t use generalizations or
               flowy words that don’t actually mean anything (well, ok maybe just
               a little bit if it's in your concluding lesson learned or ending
               message thingy).
@@ -119,73 +109,68 @@ export default function EssayWritingTips() {
           </li>
           <li className={classes.infoList}>
             <Typography>
-              People like dynamic characters; show growth and highlight
-              disparity in perspective over time. Static narrators are boring.
-            </Typography>
-          </li>
-          <li className={classes.infoList}>
-            <Typography>
-              Watch your tone and mood. If the surrounding sentences are
-              creating a certain feeling, make sure sentences in between match
-              and aren’t jarring. Otherwise your reader will lose that “sucked
-              in” feeling. You need to make sure your reader stays “sucked in”
-              the whole way through.
+              People like dynamic characters, so try to show growth and highlight
+              some disparity in perspective or character over time.
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
               Parallelism helps with flow and creates rhythm. Use it to draw out
               similarities. Use it to draw out differences. Use it to make your
-              writing beautiful (see what i did there? :P).
+              writing beautiful (see? :P).
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
               Sometimes when writing about your passion, it’s easy to get lost
-              in enumerating technical details. Which is great, except I don’t
-              care about [insert your passion] (unless it’s neuroscience :)).
-              Prove to me that you are passionate about this. What are your
+              in enumerating technical details. Which is great, except your readers 
+              are unlikely to already know about [insert your passion] (unless your reader
+              is me, and it’s neuroscience :)).
+              Prove that you are passionate through your writing. What are your
               feelings about the details of these steps? Why are you so
-              interested in and what do you find beautiful about this subject or
-              these highly technical details? Make the reader fall in love with
-              [insert your passion].
+              interested in this, and what exactly do you find beautiful about this subject or
+              these highly technical details?
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
               Try not to hedge your meaning or use half-effort words/phrases
               like “I think”, “maybe”, etc unless it's necessary for the overall
-              effect or you are talking about something genuinely unknown. Be
+              effect, or you are talking about something genuinely unknown. Be
               confident.
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
-              The 2nd draft is for creating foreshadow. Go back and add hints
-              and details and little crumbs to build recurring themes that lead
+              The 2nd draft is for creating foreshadow. Go back and add little hints
+              and little details and little crumbs to build recurring themes that lead
               up to your ending.
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
-              Come full circle trick—tie the ending or concluding message back
-              to something from the beginning. This is a cheap way to leave the
-              reader with that nice cathartic feeling.
+              Use the come full circle trick—tie the ending or concluding message back
+              to something from the beginning.
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
               In general, you can always draw inspiration from ideas and
-              patterns from other authors/books that you like. Combine them to
-              develop your own writing style and distinctive voice.
+              patterns from other authors/books that you like.
+              <strike>Like Hemingway. Seriously, you should read his stuff sometime.</strike>
             </Typography>
           </li>
           <li className={classes.infoList}>
             <Typography>
               Remember that this essay is about YOU. It’s not about listing your
-              life events or talents or memories. I care much more about who you
-              are as a person—what kind of personality traits are you trying to
-              convey? What is your character and identity? What is your voice?
+              life events or talents or memories. We care much more about who you
+              are as a person—what kind of personality traits are you
+              conveying? What is your character and identity? What is your voice?
+            </Typography>
+          </li>
+          <li className={classes.infoList}>
+            <Typography>
+              Realize that I'm a neuro kid, not an author, and take everything I say with a nice grain of salt :) -paula zhu
             </Typography>
           </li>
         </ul>
